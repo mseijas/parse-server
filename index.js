9160956dc6638056549effc678206421265a4a84
Matias Seijas <matias@tanookilabs.com>
2016-03-31 08:55:18 -0400

    Changed public route to point to index.html

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.use(mountPath, api);
 // });
 
 app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/public/test.html'));
+  res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 var port = process.env.PORT || 1337;

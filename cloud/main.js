990ca527b24e03ee4152548a916f5456b8121b13
Matias Seijas <matiseijas@gmail.com>
2016-04-03 16:45:42 -0400

    Created addClosetItemForUser as a CloudCode function

--- cloud/main.js
+++ cloud/main.js
@@ -2,3 +2,45 @@
 Parse.Cloud.define('hello', function(req, res) {
   res.success('Hi');
 });
+
+Parse.Cloud.define('addClosetItemForUser', function(req, res) {
+	var ClothingType = Parse.Object.extend("User_ClosetItem")
+
+	var user = request.params.user
+	var beaconMajor = request.params.beaconMajor
+	var beaconMinor = request.params.beaconMajor
+
+	var brandQuery = new Parse.Query("Clothing_Brand")
+	brandQuery.equalTo("beaconMajor", beaconMajor)
+
+	var clothingQuery = new Parse.Query("Clothing")
+	clothingQuery.equalTo("beaconMinor", beaconMinor)
+	clothingQuery.matchesQuery("brand", brandQuery)
+
+	query.find({
+	    success: function(results) {
+		    var clothing = results[0]
+		    if (clothing == null) {
+		    	response.error("Clothing could not be found in databse.");
+		    	return;
+		    }
+
+		    var closetItem = new ClothingType()
+		    closetItem.set("user", user)
+		    closetItem.set("clothing", clothing)
+		    closetItem.set("isActive", true)
+
+		    closetItem.save(null, {
+			    success: function(closetItem) {
+			      response.success(closetItem)
+			    },
+			    error: function(error) {
+			      response.error(error);
+			    }
+			})
+	    },
+	    error: function(error) {
+			response.error(error);
+	    }
+	});
+});

3d6b1650f3797e89d0bf943754c90faa146bbeb1
Matias Seijas <matiseijas@gmail.com>
2016-05-01 19:53:51 -0400

    Fixed bug where outwearScores == 0

--- cloud/main.js
+++ cloud/main.js
@@ -259,21 +259,16 @@ function generateSuggestionFor(aiScores, allTops, allBottoms, allOuterwear) {
   var bottom = generateClothingSuggestionFrom(bottomScores)[0]
   var outerwear
 
-  // if (aiScores[0].get("tempRange")[0] >= 17) {
-  // 	console.log("tempRange >= 17")
-
-  //   var shouldOfferOuterwear = getRandomInt(0,1)
-  //   console.log("shouldOfferOuterwear: " + shouldOfferOuterwear)
-
-  //   if (shouldOfferOuterwear == 1) {
-  //   	console.log("shouldOfferOuterwear == 1")
-  //     outerwear = generateClothingSuggestionFrom(outerwearScores)[0]
-  //   }
-  // } else {
-  // 	console.log("tempRange < 17")
-  // 	outerwear = generateClothingSuggestionFrom(outerwearScores)[0]
-  // }
-  outerwear = generateClothingSuggestionFrom(outerwearScores)[0]
+  if (outerwearScores > 0) {
+  	if (aiScores[0].get("tempRange")[0] >= 17) {
+	    var shouldOfferOuterwear = getRandomInt(0,1)
+	    if (shouldOfferOuterwear == 1) {
+	      outerwear = generateClothingSuggestionFrom(outerwearScores)[0]
+	    }
+	  } else {
+	  	outerwear = generateClothingSuggestionFrom(outerwearScores)[0]
+	  }
+  } 
 
   console.log(suggestion)
   var suggestion = {
